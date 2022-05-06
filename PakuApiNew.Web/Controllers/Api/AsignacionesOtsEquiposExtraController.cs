823cbf16be83b9fcd3b47b688f8ed793b89a3a78
LuisPulenta <luisalbertonu@gmail.com>
2022-05-06 14:05:28 -0300

    EquiposExtra

---
+++ PakuApiNew.Web/Controllers/Api/AsignacionesOtsEquiposExtraController.cs
@@ -0,0 +1,58 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PakuApiNew.Web.Data;
+using PakuApiNew.Web.Data.Entities;
+using PakuApiNew.Web.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fleet_App.Web.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AsignacionesOtsEquiposExtraController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+
+        public AsignacionesOtsEquiposExtraController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostAsignacionesOtsEquiposExtra([FromBody] AsignacionesOtsEquiposExtra request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _dataContext.AsignacionesOtsEquiposExtras.Add(request);
+            await _dataContext.SaveChangesAsync();
+
+            var newAsignacionesOtsEquiposExtra = await _dataContext
+                .AsignacionesOtsEquiposExtras
+                .Where(a => 
+                    a.NROCLIENTE == request.NROCLIENTE 
+                    && a.NROSERIEEXTRA == request.NROSERIEEXTRA)
+                .ToListAsync();
+
+            var response = new List<AsignacionesOtsEquiposExtraRequest>(newAsignacionesOtsEquiposExtra.Select(o => new AsignacionesOtsEquiposExtraRequest
+            {
+                CODDECO1=o.CODDECO1,
+                FECHACARGA=o.FECHACARGA,
+                IDASIGNACIONEXTRA = o.IDASIGNACIONEXTRA,
+                IDGAOS=o.IDGAOS,
+                IDTECNICO = o.IDTECNICO,
+                NROCLIENTE=o.NROCLIENTE,
+                NROSERIEEXTRA = o.NROSERIEEXTRA,
+                ProyectoModulo = o.ProyectoModulo
+            }).ToList());
+
+
+            return Ok(response);
+        }
+    }
+}
