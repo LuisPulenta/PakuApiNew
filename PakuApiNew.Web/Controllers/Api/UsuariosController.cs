9b153ba5c8af1727cc34c14096446615c889a173
LuisPulenta <luisalbertonu@gmail.com>
2022-12-03 08:59:29 -0300

    GetAsignacionesTodas

--- PakuApiNew.Web/Controllers/Api/UsuariosController.cs
+++ PakuApiNew.Web/Controllers/Api/UsuariosController.cs
@@ -28,7 +28,7 @@ namespace PakuApiNew.Web.Controllers.Api
             }
 
             System.Collections.Generic.List<Usuario> usuarios = await _dataContext.SubContratistasUsrWebs
-           .Where (o=> (o.CODIGO=="PQ" || o.CODIGO == "TR") && (o.HabilitadoWeb==1)) 
+           .Where(o => (o.CODIGO == "PQ" || o.CODIGO == "TR") && (o.HabilitadoWeb == 1))
            .ToListAsync();
             return Ok(usuarios);
         }
@@ -43,7 +43,7 @@ namespace PakuApiNew.Web.Controllers.Api
             }
 
             var rutas = await _dataContext.p_Rutas2
-                .Include (p => p.Paradas)
+                .Include(p => p.Paradas)
                 .Include(e => e.Envios)
            .Where(o => (o.IDUser == IDUser) && (o.Estado == 0))
 
@@ -68,7 +68,7 @@ namespace PakuApiNew.Web.Controllers.Api
             }
 
             var tipos = await _dataContext.AsignacionesOTs2
-                
+
            .Where(o => (o.UserID == UserID) && (o.ESTADOGAOS != "EJB") && (o.CierraEnAPP == 0) && (o.NoMostrarAPP == 0))
            .OrderBy(o => o.PROYECTOMODULO)
            .GroupBy(r => new
@@ -91,7 +91,7 @@ namespace PakuApiNew.Web.Controllers.Api
 
         [HttpPost]
         [Route("GetZonas/{UserID}/{ProyectoModulo}")]
-        public async Task<IActionResult> GetZonas(int UserID,string ProyectoModulo)
+        public async Task<IActionResult> GetZonas(int UserID, string ProyectoModulo)
         {
             if (!ModelState.IsValid)
             {
@@ -225,7 +225,7 @@ namespace PakuApiNew.Web.Controllers.Api
                CP = g.Key.CP,
                ENTRECALLE1 = g.Key.ENTRECALLE1,
                ENTRECALLE2 = g.Key.ENTRECALLE2,
-               Partido =g.Key.Partido,
+               Partido = g.Key.Partido,
                LOCALIDAD = g.Key.LOCALIDAD,
                TELEFONO = g.Key.TELEFONO,
                GRXX = g.Key.GRXX,
@@ -243,7 +243,131 @@ namespace PakuApiNew.Web.Controllers.Api
                Novedades = g.Key.Novedades,
                PROVINCIA = g.Key.PROVINCIA,
                ReclamoTecnicoID = g.Key.ReclamoTecnicoID,
-               Motivos=g.Key.Motivos,
+               Motivos = g.Key.Motivos,
+               FechaCita = g.Key.FechaCita,
+               MedioCita = g.Key.MedioCita,
+               NroSeriesExtras = g.Key.NroSeriesExtras,
+               Evento1 = g.Key.Evento1,
+               FechaEvento1 = g.Key.FechaEvento1,
+               Evento2 = g.Key.Evento2,
+               FechaEvento2 = g.Key.FechaEvento2,
+               Evento3 = g.Key.Evento3,
+               FechaEvento3 = g.Key.FechaEvento3,
+               Evento4 = g.Key.Evento4,
+               FechaEvento4 = g.Key.FechaEvento4,
+               Observacion = g.Key.Observacion,
+               TelefAlternativo1 = g.Key.TelefAlternativo1,
+               TelefAlternativo2 = g.Key.TelefAlternativo2,
+               TelefAlternativo3 = g.Key.TelefAlternativo3,
+               TelefAlternativo4 = g.Key.TelefAlternativo4,
+               //ObservacionCaptura=g.Key.ObservacionCaptura,
+               ZONA = g.Key.ZONA,
+
+
+               CantAsign = g.Count(),
+           })
+        .ToListAsync();
+
+
+            if (orders == null)
+            {
+                return BadRequest("No hay Asignaciones para este Usuario.");
+            }
+
+            return Ok(orders);
+        }
+
+        [HttpPost]
+        [Route("GetAsignacionesTodas/{UserID}")]
+        public async Task<IActionResult> GetAsignacionesTodas(int UserID)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var orders = await _dataContext.AsignacionesOTs2
+
+           .Where(o => (o.UserID == UserID)
+                        && (o.CierraEnAPP == 0) && (o.NoMostrarAPP == 0)
+                        && (o.BAJASISTEMA == 0)
+                        )
+           .OrderBy(o => o.RECUPIDJOBCARD)
+           .GroupBy(r => new
+           {
+               //r.RECUPIDJOBCARD,
+               r.CLIENTE,
+               r.NOMBRE,
+               r.DOMICILIO,
+               r.CP,
+               r.ENTRECALLE1,
+               r.ENTRECALLE2,
+               r.Partido,
+               r.LOCALIDAD,
+               r.TELEFONO,
+               r.GRXX,
+               r.GRYY,
+               r.ESTADOGAOS,
+               r.PROYECTOMODULO,
+               r.UserID,
+               r.CAUSANTEC,
+               r.SUBCON,
+               r.FechaAsignada,
+               r.CodigoCierre,
+               r.DESCRIPCION,
+               r.CierraEnAPP,
+               r.NoMostrarAPP,
+               r.Novedades,
+               r.PROVINCIA,
+               r.ReclamoTecnicoID,
+               r.Motivos,
+               r.FechaCita,
+               r.MedioCita,
+               r.NroSeriesExtras,
+               r.Evento1,
+               r.FechaEvento1,
+               r.Evento2,
+               r.FechaEvento2,
+               r.Evento3,
+               r.FechaEvento3,
+               r.Evento4,
+               r.FechaEvento4,
+               r.Observacion,
+               r.TelefAlternativo1,
+               r.TelefAlternativo2,
+               r.TelefAlternativo3,
+               r.TelefAlternativo4,
+               //r.ObservacionCaptura,
+               r.ZONA
+           })
+           .Select(g => new
+           {
+               //RECUPIDJOBCARD = g.Key.RECUPIDJOBCARD,
+               CLIENTE = g.Key.CLIENTE,
+               NOMBRE = g.Key.NOMBRE,
+               DOMICILIO = g.Key.DOMICILIO,
+               CP = g.Key.CP,
+               ENTRECALLE1 = g.Key.ENTRECALLE1,
+               ENTRECALLE2 = g.Key.ENTRECALLE2,
+               Partido = g.Key.Partido,
+               LOCALIDAD = g.Key.LOCALIDAD,
+               TELEFONO = g.Key.TELEFONO,
+               GRXX = g.Key.GRXX,
+               GRYY = g.Key.GRYY,
+               ESTADOGAOS = g.Key.ESTADOGAOS,
+               PROYECTOMODULO = g.Key.PROYECTOMODULO,
+               UserID = g.Key.UserID,
+               CAUSANTEC = g.Key.CAUSANTEC,
+               SUBCON = g.Key.SUBCON,
+               FechaAsignada = g.Key.FechaAsignada,
+               CodigoCierre = g.Key.CodigoCierre,
+               DESCRIPCION = g.Key.DESCRIPCION,
+               CierraEnAPP = g.Key.CierraEnAPP,
+               NoMostrarAPP = g.Key.NoMostrarAPP,
+               Novedades = g.Key.Novedades,
+               PROVINCIA = g.Key.PROVINCIA,
+               ReclamoTecnicoID = g.Key.ReclamoTecnicoID,
+               Motivos = g.Key.Motivos,
                FechaCita = g.Key.FechaCita,
                MedioCita = g.Key.MedioCita,
                NroSeriesExtras = g.Key.NroSeriesExtras,
@@ -288,7 +412,7 @@ namespace PakuApiNew.Web.Controllers.Api
 
             var codigoscierre = await _dataContext.CodigosCierre
 
-           .Where(o => (o.PROYECTOMODULO == ProyectoModulo) && (o.NoMostrarAPP==0))
+           .Where(o => (o.PROYECTOMODULO == ProyectoModulo) && (o.NoMostrarAPP == 0))
            .OrderBy(o => o.CodigoCierre)
            .ToListAsync();
 
