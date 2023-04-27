e5e4ae14ec57238aecedc6f6c21b20845fd2a6e1
LuisPulenta <luisalbertonu@gmail.com>
2023-04-26 21:54:00 -0300

    getAsignacionesEjb

--- PakuApiNew.Web/Controllers/Api/UsuariosController.cs
+++ PakuApiNew.Web/Controllers/Api/UsuariosController.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using PakuApiNew.Web.Data;
 using PakuApiNew.Web.Data.Entities;
 using PakuApiNew.Web.Models;
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -733,6 +734,149 @@ namespace PakuApiNew.Web.Controllers.Api
             _dataContext.SubContratistasUsrWebs.Update(oldUsuario);
             await _dataContext.SaveChangesAsync();
             return Ok();
+        }
+
+        [HttpPost]
+        [Route("getAsignacionesEjb/{UserID}")]
+        public async Task<IActionResult> getAsignacionesEjb(int UserID)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            String _year = DateTime.Today.Year.ToString();
+            String _month = DateTime.Today.Month.ToString();
+            String _day = DateTime.Today.Day.ToString();
+            if (_month.Length==1)
+            {
+                _month = "0" + _month;
+            }
+            if (_day.Length == 1)
+            {
+                _day = "0" + _day;
+            }
+            String hoy = _year + "-" + _month + "-" + _day;
+
+
+            var orders = await _dataContext.AsignacionesOTs2
+
+           .Where(o => (o.UserID == UserID)
+                        && (o.FECHACUMPLIDA.ToString().Substring(0,10) == hoy)
+                        && (o.ESTADOGAOS == "EJB")
+                        )
+           .OrderBy(o => o.RECUPIDJOBCARD)
+           .GroupBy(r => new
+           {
+               r.CLIENTE,
+               r.FECHACUMPLIDA,
+               r.Documento,
+               r.NOMBRE,
+               r.DOMICILIO,
+               r.CP,
+               r.ENTRECALLE1,
+               r.ENTRECALLE2,
+               r.Partido,
+               r.LOCALIDAD,
+               r.TELEFONO,
+               r.GRXX,
+               r.GRYY,
+               r.ESTADOGAOS,
+               r.PROYECTOMODULO,
+               r.UserID,
+               r.CAUSANTEC,
+               r.SUBCON,
+               r.FechaAsignada,
+               r.CodigoCierre,
+               r.DESCRIPCION,
+               r.CierraEnAPP,
+               r.NoMostrarAPP,
+               r.Novedades,
+               r.PROVINCIA,
+               r.ReclamoTecnicoID,
+               r.Motivos,
+               r.FechaCita,
+               r.MedioCita,
+               r.NroSeriesExtras,
+               r.Evento1,
+               r.FechaEvento1,
+               r.Evento2,
+               r.FechaEvento2,
+               r.Evento3,
+               r.FechaEvento3,
+               r.Evento4,
+               r.FechaEvento4,
+               r.Observacion,
+               r.TelefAlternativo1,
+               r.TelefAlternativo2,
+               r.TelefAlternativo3,
+               r.TelefAlternativo4,
+               //r.ObservacionCaptura,
+               r.ZONA,
+               //r.ModificadoApp
+           })
+           .Select(g => new
+           {
+               CLIENTE = g.Key.CLIENTE,
+               FECHACUMPLIDA=g.Key.FECHACUMPLIDA,
+               Documento = g.Key.Documento,
+               NOMBRE = g.Key.NOMBRE,
+               DOMICILIO = g.Key.DOMICILIO,
+               CP = g.Key.CP,
+               ENTRECALLE1 = g.Key.ENTRECALLE1,
+               ENTRECALLE2 = g.Key.ENTRECALLE2,
+               Partido = g.Key.Partido,
+               LOCALIDAD = g.Key.LOCALIDAD,
+               TELEFONO = g.Key.TELEFONO,
+               GRXX = g.Key.GRXX,
+               GRYY = g.Key.GRYY,
+               ESTADOGAOS = g.Key.ESTADOGAOS,
+               PROYECTOMODULO = g.Key.PROYECTOMODULO,
+               UserID = g.Key.UserID,
+               CAUSANTEC = g.Key.CAUSANTEC,
+               SUBCON = g.Key.SUBCON,
+               FechaAsignada = g.Key.FechaAsignada,
+               CodigoCierre = g.Key.CodigoCierre,
+               DESCRIPCION = g.Key.DESCRIPCION,
+               CierraEnAPP = g.Key.CierraEnAPP,
+               NoMostrarAPP = g.Key.NoMostrarAPP,
+               Novedades = g.Key.Novedades,
+               PROVINCIA = g.Key.PROVINCIA,
+               ReclamoTecnicoID = g.Key.ReclamoTecnicoID,
+               Motivos = g.Key.Motivos,
+               FechaCita = g.Key.FechaCita,
+               MedioCita = g.Key.MedioCita,
+               NroSeriesExtras = g.Key.NroSeriesExtras,
+               Evento1 = g.Key.Evento1,
+               FechaEvento1 = g.Key.FechaEvento1,
+               Evento2 = g.Key.Evento2,
+               FechaEvento2 = g.Key.FechaEvento2,
+               Evento3 = g.Key.Evento3,
+               FechaEvento3 = g.Key.FechaEvento3,
+               Evento4 = g.Key.Evento4,
+               FechaEvento4 = g.Key.FechaEvento4,
+               Observacion = g.Key.Observacion,
+               TelefAlternativo1 = g.Key.TelefAlternativo1,
+               TelefAlternativo2 = g.Key.TelefAlternativo2,
+               TelefAlternativo3 = g.Key.TelefAlternativo3,
+               TelefAlternativo4 = g.Key.TelefAlternativo4,
+               //ObservacionCaptura=g.Key.ObservacionCaptura,
+               ZONA = g.Key.ZONA,
+               //ModificadoApp = g.Key.ModificadoApp,
+
+
+               CantAsign = g.Count(),
+           })
+           .OrderByDescending(o => (o.FECHACUMPLIDA))
+        .ToListAsync();
+
+
+                if (orders == null)
+                {
+                    return BadRequest("No hay Asignaciones para este Usuario.");
+                }
+
+                return Ok(orders);
         }
     }
 }
