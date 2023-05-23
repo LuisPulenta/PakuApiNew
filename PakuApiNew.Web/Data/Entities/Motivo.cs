8d328c4b0549eeffc3f5243983b91ecf00f6538c
LuisPulenta <luisalbertonu@gmail.com>
2023-05-23 10:45:03 -0300

    Campos nuevos en Motivo

--- PakuApiNew.Web/Data/Entities/Motivo.cs
+++ PakuApiNew.Web/Data/Entities/Motivo.cs
@@ -8,5 +8,7 @@ namespace PakuApiNew.Web.Data.Entities
         public int ID { get; set; }
         public string MOTIVO { get; set; }
         public int MuestraParaEntregado { get; set; }
+        public int ExclusivoCliente { get; set; }
+        public int Activo { get; set; }
     }
 }
