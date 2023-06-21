d8a3ee5d73343fc00f1978cda77de9406680b771
LuisPulenta <luisalbertonu@gmail.com>
2023-06-21 15:53:25 -0300

    Cambio en agrupacion para Prisma y SuperC

--- PakuApiNew.Web/Controllers/Api/UsuariosController.cs
+++ PakuApiNew.Web/Controllers/Api/UsuariosController.cs
@@ -407,6 +407,128 @@ namespace PakuApiNew.Web.Controllers.Api
 
                 return Ok(orders);
             }
+            if (ProyectoModulo == "Prisma" || ProyectoModulo == "SuperC")
+            {
+                var orders = await _dataContext.AsignacionesOTs2
+
+          .Where(o => (o.UserID == UserID)
+                       && (o.PROYECTOMODULO == ProyectoModulo)
+                       && (o.CierraEnAPP == 0) && (o.NoMostrarAPP == 0)
+                       && (o.BAJASISTEMA == 0)
+                       && (o.ESTADOGAOS != "EJB")
+                       )
+          .OrderBy(o => o.RECUPIDJOBCARD)
+          .GroupBy(r => new
+          {
+              //r.RECUPIDJOBCARD,
+              r.CLIENTE,
+              r.Documento,
+              r.NOMBRE,
+              r.DOMICILIO,
+              r.CP,
+              r.ENTRECALLE1,
+              r.ENTRECALLE2,
+              r.Partido,
+              r.LOCALIDAD,
+              r.TELEFONO,
+              r.GRXX,
+              r.GRYY,
+              r.ESTADOGAOS,
+              r.PROYECTOMODULO,
+              r.UserID,
+              r.CAUSANTEC,
+              r.SUBCON,
+              r.FechaAsignada,
+              r.CodigoCierre,
+              r.DESCRIPCION,
+              r.CierraEnAPP,
+              r.NoMostrarAPP,
+              r.Novedades,
+              r.PROVINCIA,
+              //r.ReclamoTecnicoID,
+              r.Motivos,
+              r.FechaCita,
+              r.MedioCita,
+              r.NroSeriesExtras,
+              r.Evento1,
+              r.FechaEvento1,
+              r.Evento2,
+              r.FechaEvento2,
+              r.Evento3,
+              r.FechaEvento3,
+              r.Evento4,
+              r.FechaEvento4,
+              r.Observacion,
+              r.TelefAlternativo1,
+              r.TelefAlternativo2,
+              r.TelefAlternativo3,
+              r.TelefAlternativo4,
+              //r.ObservacionCaptura,
+              r.ZONA,
+              //r.ModificadoApp
+          })
+          .Select(g => new
+          {
+              //RECUPIDJOBCARD = g.Key.RECUPIDJOBCARD,
+              CLIENTE = g.Key.CLIENTE,
+              Documento = g.Key.Documento,
+              NOMBRE = g.Key.NOMBRE,
+              DOMICILIO = g.Key.DOMICILIO,
+              CP = g.Key.CP,
+              ENTRECALLE1 = g.Key.ENTRECALLE1,
+              ENTRECALLE2 = g.Key.ENTRECALLE2,
+              Partido = g.Key.Partido,
+              LOCALIDAD = g.Key.LOCALIDAD,
+              TELEFONO = g.Key.TELEFONO,
+              GRXX = g.Key.GRXX,
+              GRYY = g.Key.GRYY,
+              ESTADOGAOS = g.Key.ESTADOGAOS,
+              PROYECTOMODULO = g.Key.PROYECTOMODULO,
+              UserID = g.Key.UserID,
+              CAUSANTEC = g.Key.CAUSANTEC,
+              SUBCON = g.Key.SUBCON,
+              FechaAsignada = g.Key.FechaAsignada,
+              CodigoCierre = g.Key.CodigoCierre,
+              DESCRIPCION = g.Key.DESCRIPCION,
+              CierraEnAPP = g.Key.CierraEnAPP,
+              NoMostrarAPP = g.Key.NoMostrarAPP,
+              Novedades = g.Key.Novedades,
+              PROVINCIA = g.Key.PROVINCIA,
+              //ReclamoTecnicoID = g.Key.ReclamoTecnicoID,
+              Motivos = g.Key.Motivos,
+              FechaCita = g.Key.FechaCita,
+              MedioCita = g.Key.MedioCita,
+              NroSeriesExtras = g.Key.NroSeriesExtras,
+              Evento1 = g.Key.Evento1,
+              FechaEvento1 = g.Key.FechaEvento1,
+              Evento2 = g.Key.Evento2,
+              FechaEvento2 = g.Key.FechaEvento2,
+              Evento3 = g.Key.Evento3,
+              FechaEvento3 = g.Key.FechaEvento3,
+              Evento4 = g.Key.Evento4,
+              FechaEvento4 = g.Key.FechaEvento4,
+              Observacion = g.Key.Observacion,
+              TelefAlternativo1 = g.Key.TelefAlternativo1,
+              TelefAlternativo2 = g.Key.TelefAlternativo2,
+              TelefAlternativo3 = g.Key.TelefAlternativo3,
+              TelefAlternativo4 = g.Key.TelefAlternativo4,
+              //ObservacionCaptura=g.Key.ObservacionCaptura,
+              ZONA = g.Key.ZONA,
+              //modificadoapp = g.Key.ModificadoApp,
+
+
+              CantAsign = g.Count(),
+          })
+       .ToListAsync();
+
+
+                if (orders == null)
+                {
+                    return BadRequest("No hay Asignaciones para este Usuario.");
+                }
+
+                return Ok(orders);
+            }
 
             else
             {
