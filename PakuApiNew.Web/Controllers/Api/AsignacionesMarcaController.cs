4b057ba9a80ca65a3fe099142a1c64fd2e012479
LuisPulenta <luisalbertonu@gmail.com>
2023-08-10 11:54:01 -0300

    AsignacionesMarcaController

---
+++ PakuApiNew.Web/Controllers/Api/AsignacionesMarcaController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using PakuApiNew.Web.Data;
+using PakuApiNew.Helpers;
+using PakuApiNew.Web.Models;
+
+namespace PakuApiNew.Web.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AsignacionesMarcaController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+        private readonly IFilesHelper _fileHelper;
+
+        public AsignacionesMarcaController(DataContext dataContext, IFilesHelper fileHelper)
+        {
+            _dataContext = dataContext;
+            _fileHelper = fileHelper;
+        }
+        
+        [HttpPut("{id}")]
+        
+        public async Task<IActionResult> PutMarcado([FromRoute] int id, [FromBody] MarcadoRequest asignacion)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != asignacion.IDREGISTRO)
+            {
+                return BadRequest();
+            }
+
+            var oldasignacionesOT = await _dataContext.AsignacionesOTs.FindAsync(asignacion.IDREGISTRO);
+            if (oldasignacionesOT == null)
+            {
+                return BadRequest("El registro no existe.");
+            }
+
+            oldasignacionesOT.Marcado = asignacion.Marcado;
+
+            _dataContext.AsignacionesOTs.Update(oldasignacionesOT);
+            await _dataContext.SaveChangesAsync();
+            return Ok(asignacion);
+        }        
+    }
+}
---
+++ PakuApiNew.Web/Models/MarcadoRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace PakuApiNew.Web.Models
+{
+    public class MarcadoRequest
+    {
+        public int IDREGISTRO { get; set; }
+        public int Marcado { get; set; }
+    }
+}
