26f1f933c1040f8436ffb9379d463e75dafa8633
LuisPulenta <luisalbertonu@gmail.com>
2023-09-18 21:29:47 -0300

    PutModulo

--- PakuApiNew.Web/Controllers/Api/ModulosController.cs
+++ PakuApiNew.Web/Controllers/Api/ModulosController.cs
@@ -1,6 +1,9 @@
 ﻿using PakuApiNew.Web.Data;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
+using PakuApiNew.Web.Models;
+using PakuApiNew.Web.Data.Entities;
+using System;
 
 namespace Fleet_App.Web.Controllers.API
 {
@@ -33,6 +36,41 @@ namespace Fleet_App.Web.Controllers.API
                 return NotFound();
             }
             return Ok(modulo);
+        }
+
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutModulo(int id, ModuloRequest request)
+        {
+            if (id != request.IdModulo)
+            {
+                return BadRequest();
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            Modulo modulo = await _dataContext.Modulos.FindAsync(request.IdModulo);
+            if (modulo == null)
+            {
+                return BadRequest("El modulo no existe.");
+            }
+
+            modulo.NroVersion = request.NroVersion;
+            modulo.FechaRelease = System.DateTime.Now;
+
+            try
+            {
+                _dataContext.Modulos.Update(modulo);
+                await _dataContext.SaveChangesAsync();
+                return NoContent();
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(exception.Message);
+            }
         }
     }
 }
---
+++ PakuApiNew.Web/Models/ModuloRequest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace PakuApiNew.Web.Models
+{
+    public class ModuloRequest
+    {
+        public int IdModulo { get; set; }
+        public string NroVersion { get; set; }
+    }
+}
