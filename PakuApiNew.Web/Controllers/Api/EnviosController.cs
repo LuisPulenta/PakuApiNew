8d8e22228c840617cb3b678fa10eeee995a7ae66
LuisPulenta <luisalbertonu@gmail.com>
2023-11-28 09:42:16 -0300

    EnviarMailSegunEstado

--- PakuApiNew.Web/Controllers/Api/EnviosController.cs
+++ PakuApiNew.Web/Controllers/Api/EnviosController.cs
@@ -66,7 +66,10 @@ namespace PakuApiNew.Web.Controllers.Api
             envio.UrlDNI = imageUrl1;
             envio.LATITUD2 = request.LATITUD2;
             envio.LONGITUD2 = request.LONGITUD2;
-            envio.EnviarMailSegunEstado = request.EnviarMailSegunEstado;
+            if(request.EnviarMailSegunEstado != null)
+            {
+                envio.EnviarMailSegunEstado = request.EnviarMailSegunEstado;
+            }           
 
 
             try
